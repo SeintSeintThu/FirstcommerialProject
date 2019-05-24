221ced78c19fd2b092044907c4cb55e4425d3de4
Saung Hnin Phyu <saunghninphyu@ucsy.edu.mm>
2019-05-24 17:30:36 +0630

    UPDATE SEARCH

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { RecordService } from '../service/record.service';
+import { element } from '@angular/core/src/render3';
 
 @Component({
   selector: 'search-page',
@@ -12,14 +13,15 @@ import { RecordService } from '../service/record.service';
 export class SearchPage implements OnInit {
 
   constructor(
-    private fireStore : AngularFirestore,
-    private recordService : RecordService
+    private fireStore: AngularFirestore,
+    private recordService: RecordService
   ) { }
   records = [];
   afterSearch = [];
-  flag : boolean;
+  foundIds  : string[];
   ngOnInit() {
     this.afterSearch = [];
+    this.foundIds = new Array();
     let recordRef = this.fireStore.collection('record');
     recordRef.get()
       .subscribe(snapshot => {
@@ -37,40 +39,84 @@ export class SearchPage implements OnInit {
           console.log('Error getting documents', err);
         });
     console.log(this.records);
+    
   }
-  search(searchEvent: any) {
-    let term = searchEvent.target.value;
+  onEnter(value) {
+    let term = value;
     console.log(term);
     if (term.trim() === '') {
       console.log('Type a number');
       this.afterSearch.pop();
       this.afterSearch = [];
     }
+    
     console.log(this.records)
     this.records.forEach(element => {
       console.log(element);
-      console.log(term); 
-         console.log(element.object.customerName+ ":" + term)
-         if (element.object.customerName === term || element.object.phoneNumber === term || 
-          element.object.phoneNumber === term || element.object.township === term || element.object.note === term 
-          || element.object.makeDate === term){
-            this.flag = true;
-            console.log(this.flag);
-          }
-  
-          for(let usage of element.object.usage){
-            if(usage.number === term || usage.amount === term || this.flag){
-                  let meta = {
-                      id: element.id,
-                     customerName: element.object.customerName,
-                     customerPhoneNumber: element.object.phoneNumber,
-                     number :usage.number,
-                     amount: usage.amount
-                   }
-                  this.afterSearch.push(meta);
-          }
-        }
+        console.log(term);
+        console.log(element.object.customerName + ":" + term)
+        console.log(element.object.customerName === term);
+        console.log(element.object.township === term);
+        console.log(element.object.note === term);
+      console.log(element.object.makeDate === term);     
 
-        });
-  } 
+
+        if (element.object.customerName === term){
+              this.foundIds.push(element.id);
+              console.log(this.foundIds);
+        }   
+          //this.makeAddMeta(element.id, element.object.customerName, element.object.phoneNumber, usage.number, usage.amount);
+        else if (element.object.phoneNumber === term){
+            this.foundIds.push(element.id);
+            console.log(this.foundIds);
+        }   
+          //this.makeAddMeta(element.id, element.object.customerName, element.object.phoneNumber, usage.number, usage.amount);
+        else if (element.object.township === term){
+
+            this.foundIds.push(element.id);
+            console.log(this.foundIds);
+        }   
+          //this.makeAddMeta(element.id, element.object.customerName, element.object.phoneNumber, usage.number, usage.amount);
+        else if (element.object.note === term){
+            this.foundIds.push(element.id);
+            console.log(this.foundIds);
+        }   
+          //this.makeAddMeta(element.id, element.object.customerName, element.object.phoneNumber, usage.number, usage.amount);
+        else if (element.object.makeDate === term) {
+            this.foundIds.push(element.id);
+            console.log(this.foundIds);
+          } 
+
+          //this.makeAddMeta(element.id, element.object.customerName, element.object.phoneNumber, usage.number, usage.amount);
+    });
+        console.log(this.foundIds);
+      //  this.makeAddMeta(foundIds)
+
+  }
+
+  makeAddMeta(foundIds : string[]) {
+    let total = 0;
+    foundIds.forEach(id =>{
+      this.records.forEach(element =>{
+        for(let usage  of element.object.usage){
+          total = +total + +usage.amount;
+        if(element.id == id){
+          let meta = {
+            id: id,
+            customerName: element.object.customerName,
+            customerPhoneNumber: element.object.phoneNumber,
+            number: usage.number,
+            amount: usage.amount,
+            total : total
+          }
+          this.afterSearch.push(meta);
+        }
+      }
+
+    });
+    
+  });
+   
+    
+  }
 }
